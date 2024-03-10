8ea4d4b8a46edb4ce0205d4c8923b2d3e0902087
JerichoJones <markmelanson@gmail.com>
2024-03-09 21:06:02 -0500

    Update New-PSDDriverPackage_NoMDTScan.ps1
    formatting

--- New-PSDDriverPackage_NoMDTScan.ps1
+++ New-PSDDriverPackage_NoMDTScan.ps1
@@ -25,11 +25,11 @@
           Version - 0.0.2 - () - Added support for creating driverpacks based on age of driver source folders. I use the CREATION date.
                                  If you add drivers to an existing folder it will most likely NOT BE DETECTED.
                                  Create a New folder and copy everything into that.
-		  Version - 0.0.3 - () - Resolved issue where all driverpack folders were being deleted regardless of DaysOld
-								 Added output to console so we can see what is happening
+	  Version - 0.0.3 - () - Resolved issue where all driverpack folders were being deleted regardless of DaysOld
+				 Added output to console so we can see what is happening
           Version - 0.0.4 - () - Added timestamps to the output to console
                                  No longer uses the MDT driver store to determine drivers per model, recreate the folder structure under
-								 RootDriverPath to match the DriverPath in your Task Sequence!
+				 RootDriverPath to match the DriverPath in your Task Sequence!
                                  You must define the $SourceFolderNames with the driver folder structure:
                                  EXAMPLE:
                                  $SourceFolderNames = @("Windows 10 x64\WinPE X64",
@@ -51,7 +51,7 @@
                                     E:\Drivers\Windows 10 x64\VMware, Inc\VMware20,1
 
                                  and the DriverPath in your Task Sequence like:
-	                 			    Windows 10 x64\%make%\%modelalias%
+	      			    Windows 10 x64\%make%\%modelalias%
 
                                  NOTE: I use MODELALIAS because Lenovo chooses to be "different"
  
